e26daa0b4600aead9cca0abd8e1d0a061f6e4e2e
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2026-02-06 19:46:35 +0530

    feat: add code for github action

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: "/RasmiranjanSahoo-Portfolio",
+  assetPrefix: "/RasmiranjanSahoo-Portfolio/",
   images: {
     unoptimized: true,
   },
