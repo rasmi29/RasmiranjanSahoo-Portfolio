51c41d708def294750f2bf2f5339fcb737afd17a
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2026-02-06 19:59:38 +0530

    fix; soem issue

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: "/RasmiranjanSahoo-Portfolio",
-  assetPrefix: "/RasmiranjanSahoo-Portfolio/",
   images: {
     unoptimized: true,
   },
